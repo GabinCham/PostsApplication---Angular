6254eb0f9e9149fa9b223eca395554f58d031ff3
GabinCham <gabin.chameroy@hotmail.com>
2019-12-04 14:34:16 +0100

    avancement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostItemComponent } from './post-item/post-item.component';
+import { NewPostComponent } from './new-post/new-post.component';
+import {Routes} from '@angular/router';
+import { RouterModule} from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
+const appRoutes: Routes = [
+  {path: 'Posts',  component: PostListComponent},
+  {path: 'NewPost', component: NewPostComponent},
+  {path: '**', component: PostListComponent},
+  {path: '', component: PostListComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
-    PostItemComponent
+    PostItemComponent,
+    NewPostComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
